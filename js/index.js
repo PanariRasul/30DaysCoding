a158c566e321175c77c3539d94160462c1eb63aa
PanariRasul <panarirasul@gmail.com>
2024-04-26 14:25:09 +0530

    new one

---
+++ js/index.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+
+const isEqualTo100 = (a, b) =>{
+    if (a ==100 || b ==100 || a+b == 100) {
+        console.log("You Enter Number is Equal to 100");
+    }else {
+        console.log("Entered Number is Not Equal to 100")
+    }
+};
+
+isEqualTo100(100,0)
